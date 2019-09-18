f7844178c2d3bac5f51d7723c7f2e7b6948b1fb1
zhangyong <zhangyong@163.com>
2019-09-18 11:39:29 +0800

    feat(com:personal): 个人中心静态页面

--- chat_client/src/containers/personal/personal.jsx
+++ chat_client/src/containers/personal/personal.jsx
@@ -1,18 +1,29 @@
 // 用户个人中心路由组件
 import React, { Component } from 'react'
+import { Result, List, WhiteSpace, Button } from 'antd-mobile'
 
+const Item = List.Item;
+const Brief = Item.Brief;
 export default class Personal extends Component {
   render() {
     return (
       <div>
-        <p>个人中心</p>
-        <p>个人中心</p>
-        <p>个人中心</p>
-        <p>个人中心</p>
-        <p>个人中心</p>
-        <p>个人中心</p>
-        <p>个人中心</p>
-        <p>个人中心</p>
+        <Result
+          img={<img src={require(`../../assets/images/头像1.png`)} style={{ width: 50 }} alt="avatar" />}
+          title="张三"
+          message="IBM"
+        />
+        <List renderHeader={() => '相关信息'}>
+          <Item multipleLine>
+            <Brief>职位:前端工程师</Brief>
+            <Brief>简介:React/Vue/jQuery</Brief>
+            <Brief>薪资:20k</Brief>
+          </Item>
+        </List>
+        <WhiteSpace />
+        <List>
+          <Button type="warning">退出登录</Button>
+        </List>
       </div>
     )
   }
